21f3634b6e5a2b08ec4a316b92aac322818e9f71
T3Dyy1337 <juricektadeas@gmail.com>
2024-04-30 18:08:03 +0200

    User n Event

---
+++ SEP2/Client/src/model/Event.java
@@ -0,0 +1,52 @@
+package model;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+public class Event {
+    private UUID eventId;
+    private String title;
+    private String description;
+    private LocalDateTime startTime;
+    private LocalDateTime endTime;
+
+    public Event(String title, String description, LocalDateTime startTime, LocalDateTime endTime) {
+        this.eventId = UUID.randomUUID();
+        this.title = title;
+        this.description = description;
+        this.startTime = startTime;
+        this.endTime = endTime;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDateTime getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(LocalDateTime startTime) {
+        this.startTime = startTime;
+    }
+
+    public LocalDateTime getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(LocalDateTime endTime) {
+        this.endTime = endTime;
+    }
+}
---
+++ SEP2/Client/src/model/User.java
@@ -0,0 +1,38 @@
+package model;
+
+import java.util.List;
+import java.util.UUID;
+
+public class User {
+    private UUID id;
+    private String email;
+    private String password;
+
+    private List<Event> eventList;
+
+    public User(String email, String password) {
+        this.id = UUID.randomUUID();
+        this.email = email;
+        this.password = password;
+    }
+
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void addEvent(Event event){
+        eventList.add(event);
+    }
+
+
+}
