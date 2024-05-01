028b4e9fc871e6fba7f3cd543febbe603b742352
DannyMnt <munteanudaniel098@gmail.com>
2024-05-01 10:25:16 +0200

    Printing adding event

--- SEP2/Client/src/view/AddEventViewController.java
+++ SEP2/Client/src/view/AddEventViewController.java
@@ -1,5 +1,8 @@
 package view;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.Region;
 import viewmodel.AddEventViewModel;
 
@@ -7,7 +10,14 @@ public class AddEventViewController {
     private Region root;
     private ViewHandler viewHandler;
     private AddEventViewModel addEventViewModel;
-
+    @FXML
+    private TextField eventTitle;
+    @FXML
+    private TextField eventDescription;
+    @FXML
+    private DatePicker startDate;
+    @FXML
+    private DatePicker endDate;
     public AddEventViewController(){
 
     }
@@ -15,14 +25,22 @@ public class AddEventViewController {
         this.viewHandler = viewHandler;
         this.addEventViewModel = addEventViewModel;
         this.root = root;
+
+        eventTitle.textProperty().bindBidirectional(addEventViewModel.getEventTitleProperty());
+        eventDescription.textProperty().bindBidirectional(addEventViewModel.getEventDescriptionProperty());
+        startDate.valueProperty().bindBidirectional(addEventViewModel.getStartDate().valueProperty());
+        endDate.valueProperty().bindBidirectional(addEventViewModel.getEndDate().valueProperty());
     }
 
     public void reset(){
-        System.out.println("wdawdada");
 
     }
 
     public Region getRoot() {
         return root;
+    }
+
+    public void addEventBtn() {
+        addEventViewModel.addEvent();
     }
 }
