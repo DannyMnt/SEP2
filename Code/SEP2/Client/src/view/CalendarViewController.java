1a5eb32e10c2bb6b80a4c6957cd022534de7ce4f
T3Dyy1337 <juricektadeas@gmail.com>
2024-05-15 22:27:39 +0200

    iewf er ui

--- Code/SEP2/Client/src/view/CalendarViewController.java
+++ Code/SEP2/Client/src/view/CalendarViewController.java
@@ -1,8 +1,12 @@
 package view;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.Region;
+
+import java.io.IOException;
 
 public class CalendarViewController
 {
@@ -11,5 +15,20 @@ public class CalendarViewController
 
   @FXML GridPane gridPane;
 
-  
+  public void init(){
+    for (int row = 0; row < gridPane.getRowCount(); row++) {
+      for (int col = 0; col < gridPane.getColumnCount(); col++) {
+        try {
+          // Load the sub-FXML file
+          FXMLLoader loader = new FXMLLoader(getClass().getResource("dayEntryView.fxml"));
+          Pane cellContent = loader.load();
+
+          // Add the loaded content to the corresponding cell in the GridPane
+          gridPane.add(cellContent, col, row);
+        } catch (IOException e) {
+          e.printStackTrace();
+        }
+      }
+    }
+  }
 }
