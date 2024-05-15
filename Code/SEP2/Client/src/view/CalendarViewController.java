2c5d7a33d1188130becb448234efac6fa3b5ba03
T3Dyy1337 <juricektadeas@gmail.com>
2024-05-15 14:48:18 +0200

    hm

---
+++ Code/SEP2/Client/src/view/CalendarViewController.java
@@ -0,0 +1,15 @@
+package view;
+
+import javafx.fxml.FXML;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Region;
+
+public class CalendarViewController
+{
+  private Region root;
+  private ViewHandler viewHandler;
+
+  @FXML GridPane gridPane;
+
+  
+}
