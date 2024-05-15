724ac673dd1f37e8f16d2f91b3bd3441e91d17d5
T3Dyy1337 <juricektadeas@gmail.com>
2024-05-16 01:33:37 +0200

    ahh2ez

--- Code/SEP2/Client/src/view/CalendarViewController.java
+++ Code/SEP2/Client/src/view/CalendarViewController.java
@@ -1,67 +1,118 @@
 package view;
 
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.geometry.Insets;
-import javafx.scene.control.*;
+import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.Region;
-import javafx.scene.layout.VBox;
-import model.Country;
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
 import viewmodel.CalendarViewModel;
-import viewmodel.RegisterUserViewModel;
 
-import java.io.FileReader;
+
+
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.format.TextStyle;
+import java.time.temporal.TemporalAdjusters;
+import java.util.Locale;
 
-public class CalendarViewController
-{
-  private Region root;
+public class CalendarViewController {
   private ViewHandler viewHandler;
-
   private CalendarViewModel calendarViewModel;
+  private Region root;
 
-  @FXML GridPane gridPane;
+  @FXML private Label monthLabel;
 
 
 
-  public void init(ViewHandler viewHandler, CalendarViewModel calendarViewModel, Region root){
+  @FXML
+  private GridPane gridPane;
+  private LocalDate calendarStartDate;
+
+  public void init(ViewHandler viewHandler, CalendarViewModel calendarViewModel, Region root) {
     this.viewHandler = viewHandler;
     this.calendarViewModel = calendarViewModel;
     this.root = root;
 
-//    for (int row = 0; row < gridPane.getRowCount(); row++) {
-//      for (int col = 0; col < gridPane.getColumnCount(); col++) {
-//        try {
-//          // Load the sub-FXML file
-//          FXMLLoader loader = new FXMLLoader(getClass().getResource("dayEntryView.fxml"));
-//          Pane cellContent = loader.load();
-//
-//          // Add the loaded content to the corresponding cell in the GridPane
-//          gridPane.add(cellContent, col, row);
-//        } catch (IOException e) {
-//          e.printStackTrace();
-//        }
-//      }
-//    }
+    LocalDate today = LocalDate.now();
+
+    LocalDate firstDayOfMonth = today.with(TemporalAdjusters.firstDayOfMonth());
+
+    int startingDayOfWeek = firstDayOfMonth.getDayOfWeek().getValue();
+    this.calendarStartDate = firstDayOfMonth;
+    System.out.println(calendarStartDate);
+
+    // Determine the number of days to show from the previous month
+    int daysFromPreviousMonth = startingDayOfWeek - 1;
+
+    monthLabel.setText(calendarStartDate.getMonth().getDisplayName(TextStyle.FULL, Locale.getDefault()));
+
+    // Load cells for the previous month
+    LocalDate startDate = firstDayOfMonth.minusDays(daysFromPreviousMonth);
+
+    loadMonth(startDate);
+  }
+
+  private void loadMonth(LocalDate startDate) {
+    for (int row = 0; row < gridPane.getRowCount(); row++) {
+      for (int col = 0; col < gridPane.getColumnCount(); col++) {
+        try {
+          // Load the sub-FXML file
+          FXMLLoader loader = new FXMLLoader(getClass().getResource("dayEntryView.fxml"));
+          Pane cellContent = loader.load();
+
+          // Get the controller and set the date
+          DayEntryViewController controller = loader.getController();
+          controller.setDate(startDate);
+
+          // Add the loaded content to the corresponding cell in the GridPane
+          gridPane.add(cellContent, col, row);
+
+          // Update the date for the next cell
+          startDate = startDate.plusDays(1);
+        } catch (IOException e) {
+          e.printStackTrace();
+        }
+      }
+    }
+  }
+
+  @FXML
+  private void goToPreviousMonth() {
+    this.calendarStartDate = calendarStartDate.minusMonths(1);
+    DayOfWeek dayOfWeek = calendarStartDate.getDayOfWeek();
+    int daysAfterMonday = dayOfWeek.getValue() - DayOfWeek.MONDAY.getValue();
+    LocalDate firstMondayDate = calendarStartDate.minusDays(daysAfterMonday);
+
+    reset();
+    monthLabel.setText(calendarStartDate.getMonth().getDisplayName(TextStyle.FULL, Locale.getDefault()));
+
+    loadMonth(firstMondayDate);
+  }
+
+  @FXML
+  private void goToNextMonth() {
+
+    this.calendarStartDate = calendarStartDate.plusMonths(1);
+    DayOfWeek dayOfWeek = calendarStartDate.getDayOfWeek();
+    int daysAfterMonday = dayOfWeek.getValue() - DayOfWeek.MONDAY.getValue();
+    LocalDate firstMondayDate = calendarStartDate.minusDays(daysAfterMonday);
+
+    reset();
+    monthLabel.setText(calendarStartDate.getMonth().getDisplayName(TextStyle.FULL, Locale.getDefault()));
+
+    loadMonth(firstMondayDate);
 
   }
+
 
   public Region getRoot() {
     return root;
   }
 
   public void reset(){
-
+    gridPane.getChildren().clear();
   }
 
 
