9e90b8be92a91fd42591dbbfa5fb2fdf8da44d14
DannyMnt <munteanudaniel098@gmail.com>
2024-05-25 11:07:47 +0200

    for jan

--- Code/SEP2/Client/src/view/AddEventViewController.java
+++ Code/SEP2/Client/src/view/AddEventViewController.java
@@ -14,24 +14,19 @@ public class AddEventViewController {
     private Region root;
     private ViewHandler viewHandler;
     private AddEventViewModel addEventViewModel;
-    @FXML
-    private TextField eventTitle;
-    @FXML
-    private TextArea eventDescription;
-
-    @FXML
-    private TextField locationTextField;
-    @FXML
-    private DatePicker startDate;
-    @FXML
-    private DatePicker endDate;
+    @FXML private TextField eventTitle;
+    @FXML private TextArea eventDescription;
+    @FXML private TextField locationTextField;
+    @FXML private DatePicker startDate;
+    @FXML private DatePicker endDate;
     @FXML private TextField startTime;
     @FXML private TextField endTime;
-    @FXML
-    private Label errorLabel;
+    @FXML private Label errorLabel;
     @FXML private TextField participantsTextField;
     @FXML private VBox listView;
     @FXML private AnchorPane anchorPane;
+    @FXML private AnchorPane attendeesAnchorPane;
+    @FXML private VBox attendeesVBox;
     public AddEventViewController(){
 
     }
@@ -55,7 +50,7 @@ public class AddEventViewController {
         startTime.textProperty().bindBidirectional(addEventViewModel.getStartTimeProperty());
         endTime.textProperty().bindBidirectional(addEventViewModel.getEndTimeProperty());
         addEventViewModel.addListener();
-        addEventViewModel.setListView(listView, anchorPane);
+        addEventViewModel.setListView(listView, anchorPane, attendeesAnchorPane, attendeesVBox);
     }
 
     public void reset(){
