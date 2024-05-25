8173d68515d18def0edc0d737b412f157cf1bcb5
DannyMnt <munteanudaniel098@gmail.com>
2024-05-25 15:54:01 +0200

    attendees list

--- Code/SEP2/Client/src/view/AddEventViewController.java
+++ Code/SEP2/Client/src/view/AddEventViewController.java
@@ -54,6 +54,12 @@ public class AddEventViewController {
     }
 
     public void reset(){
+        eventTitle.setText(null);
+        eventDescription.setText(null);
+        startTime.setText(null);
+        endTime.setText(null);
+        locationTextField.setText(null);
+        addEventViewModel.reset();
     }
 
     public Region getRoot() {
@@ -63,9 +69,11 @@ public class AddEventViewController {
     public void addEventBtn() throws RemoteException {
         addEventViewModel.addEvent();
         viewHandler.openView("calendar");
+        reset();
     }
 
     public void cancelBtn(){
         viewHandler.openView("calendar");
+        reset();
     }
 }
